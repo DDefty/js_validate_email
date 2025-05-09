dcaae051d5c2eb6a00192371c5327a4303398a65
DDefty <dawid.kogut99@gmail.com>
2025-05-09 13:27:41 +0200

    tests added

--- src/validateEmail.test.js
+++ src/validateEmail.test.js
@@ -8,7 +8,8 @@ describe(`Function 'validateEmail':`, () => {
   });
 
   it(`should return boolean`, () => {
-
+    expect(validateEmail('test@test.test')).toBe(true);
+    expect(validateEmail('test')).toBe(false);
   });
 
   it(`should return 'true' for the valid email`, () => {
@@ -16,5 +17,39 @@ describe(`Function 'validateEmail':`, () => {
       .toBeTruthy();
   });
 
+  it(`should return 'false' for the invalid email without @`, () => {
+    expect(validateEmail('test838gmail.com.'))
+      .toBe(false);
+  });
+
+  it(`should return 'false' for the email starting with .`, () => {
+    expect(validateEmail('.test838@gmail.com.'))
+      .toBe(false);
+  });
+
+  it(`should return 'false' for the email  with double dot`, () => {
+    expect(validateEmail('t..st838@gmail.com.'))
+      .toBe(false);
+  });
+
+  it(`should return 'false' for the email  with characters`, () => {
+    expect(validateEmail('t$%%st838@gmail.com.'))
+      .toBe(false);
+  });
+
+  it(`should return 'false' for the email  with dot at the end`, () => {
+    expect(validateEmail('test.@gmail.com.'))
+      .toBe(false);
+  });
+
+  it(`should return 'true' for the email  with '-' '_' characters`, () => {
+    expect(validateEmail('te_s-t@gmail.com.'))
+      .toBe(true);
+  });
+
+  it(`should return 'false' for the email  without string after @`, () => {
+    expect(validateEmail('test@gmail'))
+      .toBe(false);
+  });
   // write more tests here
 });
